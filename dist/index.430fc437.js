81a151d05f9dca952e13448663203272e05f32d7
Jesse Naakka <jesse.naakka@gmail.com>
2022-01-06 18:41:24 +0200

    Function for getting one recipes data with fetch, dist file upload

--- dist/index.430fc437.js
+++ dist/index.430fc437.js
@@ -1,27 +1,21 @@
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
 };
-
 // https://forkify-api.herokuapp.com/v2
-
 ///////////////////////////////////////
-
 // Function for findng one recipe data test-url:https://forkify-api.herokuapp.com/api/get?rId=47746
-const showRecipe = async function(url){
-    try{
+const showRecipe = async function(url) {
+    try {
         const res = await fetch(url);
         const data = await res.json();
-
-        if(!res.ok) throw new Error(`${data.error}`);
-        
+        if (!res.ok) throw new Error(`${data.error}`);
         console.log(res, data);
-        let {recipe} = data;
+        let { recipe  } = data;
         recipe = {
             id: recipe.recipe_id,
             title: recipe.title,
@@ -31,9 +25,11 @@ const showRecipe = async function(url){
             socialRank: recipe.social_rank,
             publisherUrl: recipe.publisher_url,
             ingredients: recipe.ingredients
-        }
+        };
         console.log(recipe);
-    } catch(err){
+    } catch (err) {
         alert(err);
     }
 };
+
+//# sourceMappingURL=index.430fc437.js.map
